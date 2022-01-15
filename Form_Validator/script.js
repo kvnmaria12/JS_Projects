6cb162142c2a8bab38feaa0cee4d2a45968f8787
Kevin D <kevindemaria95@gmail.com>
2022-01-16 00:59:18 +0530

    add trim() method in ValidateEmail Function

--- Form_Validator/script.js
+++ Form_Validator/script.js
@@ -27,6 +27,7 @@ function capitalize(input) {
 // To Validate the Email
 function isValidateEmail(email) {
     return String(email)
+        .trim()
         .toLowerCase()
         .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
